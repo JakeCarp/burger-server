c1ea5cc06058e33c0ec078b3194ad43c68b69d6f
jacob carpenter <jakecarpenter407@gmail.com>
2021-11-01 15:31:58 -0600

    should be done

--- server/controllers/BurgersController.js
+++ server/controllers/BurgersController.js
@@ -1,17 +1,57 @@
-import BaseController from "../utils/BaseController";
+import { burgersService } from '../services/BurgersService'
+import BaseController from '../utils/BaseController'
 export class BurgersController extends BaseController {
-    constructor() {
-        super('api/burgers')
-        this.router
-            .get('', this.getAllBurgers)
-            .post('', this.createBurger)
-            .delete('/:burgerId', this.deleteBurger)
-            .put('/:burgerId', this.updateBurger)
-    }
-    /**
+  constructor() {
+    super('api/burgers')
+    this.router
+      .get('', this.getAllBurgers)
+      .post('', this.createBurger)
+      .delete('/:burgerId', this.deleteBurger)
+      .put('/:burgerId', this.updateBurger)
+  }
+
+  /**
    * Sends found values to a client by request
    * @param {import("express").Request} req
    * @param {import("express").Response} res
    * @param {import("express").NextFunction} next
    */
+  async getAllBurgers(req, res, next) {
+    try {
+      const burgers = await burgersService.getAllBurgers()
+      return res.send(burgers)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createBurger(req, res, next) {
+    try {
+      const burger = await burgersService.createBurger(req.body)
+      return res.send(burger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async deleteBurger(req, res, next) {
+    try {
+      const burger = await burgersService.deleteBurger(req.params.burgerId)
+      return res.send(burger)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async updateBurger(req, res, next) {
+    try {
+      const id = req.params.burgerId
+      const burgerData = req.body
+      burgerData.id = id
+      const burger = burgersService.updateBurger(id, burgerData)
+      res.send(burger)
+    } catch (error) {
+      next(error)
+    }
+  }
 }
---
+++ server/services/BurgersService.js
@@ -0,0 +1,48 @@
+
+import { BadRequest } from '../utils/Errors'
+
+const FakeDB = {
+  burgers: [
+    {
+      name: 'Big burger',
+      id: 1
+    },
+    {
+      name: 'Small burger',
+      id: 2
+    }
+  ]
+}
+
+class BurgersService {
+  async getAllBurgers() {
+    const burgers = await FakeDB.burgers
+    return burgers
+  }
+
+  async createBurger(burgerData) {
+    burgerData.id = FakeDB.burgers.length
+    await FakeDB.burgers.push(burgerData)
+    return burgerData
+  }
+
+  async deleteBurger(id) {
+    const index = FakeDB.burgers.findIndex(b => b.id === id)
+    if (index === -1) {
+      throw new BadRequest("can't delete a burger that is already not there")
+    }
+    FakeDB.burgers.splice(index, 1)
+    return 'deleted'
+  }
+
+  async updateBurger(id, burgerData) {
+    const index = FakeDB.burgers.findIndex(b => b.id === id)
+    if (index === -1) {
+      throw new BadRequest("no burger by that id")
+    }
+    FakeDB.splice(index, 1, burgerData)
+    return burgerData
+  }
+}
+
+export const burgersService = new BurgersService()
